390e956a7054c509ce5c93ad4376c980f758463f
alien-robot <161760798+alien-robot@users.noreply.github.com>
2024-04-01 20:17:32 +0000

    second try

--- pop.py
+++ pop.py
@@ -1,8 +1,12 @@
-file = open("you're gae.txt", "r+")
-word = gae 
-import webbrowser
-for i in file:
-     if any(file for c in word):
-          webbrowser.open('http://gmail.com')
+def main(): 
+   file = open("you're gae.txt", "r+")
+   word = gae 
+   import webbrowser
+   for i in file:
+      if any(file for c in word):
+            webbrowser.open('http://gmail.com')
+
+if __name__=="__main__": 
+    main() 
       
 
